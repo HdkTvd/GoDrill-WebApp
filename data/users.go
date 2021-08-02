0092ea521dd6a9136760f31688b2a72f7204ed2a
HardRock <40635659+HdkTvd@users.noreply.github.com>
2021-08-02 12:46:18 +0530

    Create users.go

---
+++ data/users.go
@@ -0,0 +1,77 @@
+package data
+
+import (
+	"fmt"
+	"strconv"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type UUID interface{}
+
+type User struct {
+	UUID        UUID   `json:"uuid"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	PhoneNumber int    `json:"phone_number"`
+	IsActive    bool   `json:"isActive"`
+}
+
+// var userMap map[UUID]*User
+
+var userList []*User
+
+type Users []*User
+
+func GetUserList() Users {
+	return userList
+}
+
+// func GetUserNames() []string {
+// 	var names []string
+
+// 	for _, u := range userList {
+// 		names = append(names, u.Name)
+// 	}
+
+// 	return names
+// }
+
+// func GetUserMap() map[UUID]*User {
+// 	return userMap
+// }
+
+func AddUser(u *User) {
+	userList = append(userList, u)
+	// userMap[u.UUID] = u
+}
+
+func getNewID() UUID {
+	id := uuid.NewV4()
+	return id
+}
+
+func (u *User) IsValid() error {
+
+	var ErrNameNotFound = fmt.Errorf("Name is required")
+	var ErrEmailNotFound = fmt.Errorf("Email is required")
+	var ErrInvalidPhoneNumber = fmt.Errorf("Invalid Phone Number")
+
+	if u.UUID == "" {
+		u.UUID = getNewID()
+	}
+
+	if u.Name == "" {
+		return ErrNameNotFound
+	}
+
+	if u.Email == "" {
+		return ErrEmailNotFound
+	}
+
+	if len(strconv.Itoa(u.PhoneNumber)) != 10 {
+		return ErrInvalidPhoneNumber
+	}
+
+	return nil
+}
